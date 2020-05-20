f81aedcee8a9806dd9cdded4a22eabba976959e7
wesh9999 <wesh9999@gmail.com>
2020-05-20 15:25:01 -0400

    working on bluetooth device discovery

--- app/src/main/java/org/weshley/fishtracker/BluetoothTestFragment.java
+++ app/src/main/java/org/weshley/fishtracker/BluetoothTestFragment.java
@@ -2,21 +2,37 @@ package org.weshley.fishtracker;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class BluetoothTestFragment
    extends AbstractFragment
 {
+
    private ViewGroup _rootView = null;
    private BluetoothAdapter _btAdapter = null;
+   private BroadcastReceiver _broadcastReceiver = null;
+   private Map<String,String> _devices = null;
+      // maps MAC addr to device name
+   private boolean _scanning = false;
 
    public static String getLabel()
    {
@@ -30,44 +46,116 @@ public class BluetoothTestFragment
       _rootView = (ViewGroup) inflater.inflate(
          R.layout.bluetooth_test_fragment, container, false);
 
+      initBluetoothState();
       initUi();
+      initDeviceDiscovery();
+      initControllers();
       return _rootView;
+   }
+
+   @Override
+   public void onDestroy()
+   {
+      super.onDestroy();
+      stopBroadcastReceiver();
+   }
+
+   @Override
+   public void onResume()
+   {
+      super.onResume();
+      initDeviceDiscovery();
+   }
+
+   @Override
+   public void onPause()
+   {
+      super.onPause();
+      stopBroadcastReceiver();
+   }
+
+   private void stopBroadcastReceiver()
+   {
+      if(null != _btAdapter)
+         _btAdapter.cancelDiscovery();
+
+      if(null != _broadcastReceiver)
+         getContext().unregisterReceiver(_broadcastReceiver);
+      _broadcastReceiver = null;
    }
 
    private void initUi()
    {
-      initBluetoothState();
-      listPairedDevices();
+      initPairedDevicesList();
+      initScanControls();
    }
 
-   private void listPairedDevices()
+   private void initControllers()
    {
-      String pairedData = "";
-      if(null == _btAdapter)
+      getScanButton().setOnClickListener(new View.OnClickListener()
       {
-         pairedData = "No Bluetooth";
-      }
-      else
-      {
-         Set<BluetoothDevice> pairedDevices = _btAdapter.getBondedDevices();
-         for (BluetoothDevice device : pairedDevices)
+         @Override
+         public void onClick(View view)
          {
-            String deviceName = device.getName();
-            //String deviceHardwareAddress = device.getAddress(); // MAC address
-            pairedData = pairedData + deviceName + "\n";
+            toggleScanning();
          }
-      }
-      getNotesField().setText(pairedData);
+      });
+   }
+
+   private void initDeviceDiscovery()
+   {
+      if(!hasBluetoothSupport())
+         return;
+
+      // Create a BroadcastReceiver for ACTION_FOUND.
+      _broadcastReceiver = new BroadcastReceiver()
+      {
+         public void onReceive(Context context, Intent intent)
+         {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+               BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+               // If it's already paired, skip it, because it's been listed already
+               String deviceName = device.getName();
+               String macAddress = device.getAddress();
+               if(device.getBondState() != BluetoothDevice.BOND_BONDED)
+                  _devices.put(macAddress, deviceName);
+               logMessage("Discovered device '" + deviceName + "'");
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action))
+            {
+               logMessage("++++ Discovery started");
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+            {
+               logMessage("++++ Discovery finished");
+            }
+         }
+      };
+
+      // Register for broadcasts when a device is discovered.
+      IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+      filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+      filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+      getContext().registerReceiver(_broadcastReceiver, filter);
+   }
+
+   private void logMessage(String msg)
+   {
+      EditText notes = getNotesField();
+      notes.setText(notes.getText() + "\n" + msg);
    }
 
    private void initBluetoothState()
    {
-      _btAdapter = getBluetoothAdapter();
+      _btAdapter = BluetoothAdapter.getDefaultAdapter();
       if(null == _btAdapter)
       {
          getBluetoothStatusText().setText("Bluetooth NOT SUPPORTED!");
          return;
       }
+
       if(_btAdapter.isEnabled())
       {
          getBluetoothStatusText().setText("Bluetooth enabled");
@@ -80,9 +168,99 @@ public class BluetoothTestFragment
       }
    }
 
-   private BluetoothAdapter getBluetoothAdapter()
+   private boolean hasBluetoothSupport()
    {
-      return BluetoothAdapter.getDefaultAdapter();
+      return null != _btAdapter;
+   }
+
+   private void initPairedDevicesList()
+   {
+      // sort the names
+      Set<String> deviceNames = new TreeSet<String>();
+      deviceNames.addAll(getPairedDevices().values());
+
+      List<String> pairedList = new ArrayList<>();
+      pairedList.addAll(deviceNames);
+
+      Spinner spinner = getPairedDevicesList();
+      ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(
+         this.getContext(), R.layout.spinner_item, pairedList);
+      spinner.setAdapter(spinnerArrayAdapter);
+   }
+
+   private Map<String,String> getPairedDevices()
+   {
+      if(null != _devices)
+         return _devices;
+      _devices = new HashMap<>();
+      if(hasBluetoothSupport())
+      {
+         Set<BluetoothDevice> pairedDevices = _btAdapter.getBondedDevices();
+         for (BluetoothDevice device : pairedDevices)
+         {
+            String deviceName = device.getName();
+            String macAddress = device.getAddress();
+            _devices.put(macAddress, deviceName);
+         }
+      }
+      return _devices;
+   }
+
+   private void initScanControls()
+   {
+      if(!hasBluetoothSupport())
+      {
+         getScanButton().setEnabled(false);
+         getScanLabel().setText("");
+         return;
+      }
+
+      getScanButton().setEnabled(true);
+      if(_scanning)
+      {
+         getScanButton().setText(R.string.bluetooth_cancel_scan_label);
+         getScanLabel().setText("Scanning...");
+      }
+      else
+      {
+         getScanButton().setText(R.string.bluetooth_scan_label);
+         getScanLabel().setText("");
+      }
+   }
+
+   private void toggleScanning()
+   {
+      if(!hasBluetoothSupport())
+         return;
+
+      if(_scanning)
+      {
+         if(!_btAdapter.cancelDiscovery())
+            logMessage("cancel discovery failed");
+         else
+            _scanning = false;
+      }
+      else
+      {
+//         if(!_btAdapter.startDiscovery())
+//            logMessage("start discovery failed");
+//         else
+//            _scanning = true;
+         _btAdapter.cancelDiscovery();
+         _btAdapter.startDiscovery();
+         _scanning = true;
+      }
+      initScanControls();
+   }
+
+   private Button getScanButton()
+   {
+      return (Button) _rootView.findViewById(R.id.bluetooth_scan_button);
+   }
+
+   private TextView getScanLabel()
+   {
+      return (TextView) _rootView.findViewById(R.id.bluetooth_scan_label);
    }
 
    private TextView getBluetoothStatusText()
@@ -93,5 +271,10 @@ public class BluetoothTestFragment
    private EditText getNotesField()
    {
       return (EditText) _rootView.findViewById(R.id.bluetooth_notes_field);
+   }
+
+   private Spinner getPairedDevicesList()
+   {
+      return (Spinner) _rootView.findViewById(R.id.bluetooth_pairs_list);
    }
 }
--- app/src/main/java/org/weshley/fishtracker/MainActivity.java
+++ app/src/main/java/org/weshley/fishtracker/MainActivity.java
@@ -1,14 +1,18 @@
 package org.weshley.fishtracker;
 
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager.widget.ViewPager;
 
@@ -16,6 +20,8 @@ public class MainActivity
    extends FragmentActivity
    implements TripListChangeListener
 {
+   private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
    private static Resources _resources = null;
    private static Context _context = null;
 
@@ -49,6 +55,7 @@ public class MainActivity
       UiManager.instance().setPager(pager, adapter);
 
       registerEventHandlers();
+      checkLocationPermission();
    }
 
    private void registerEventHandlers()
@@ -146,5 +153,79 @@ public class MainActivity
       return (Button) findViewById(R.id.fish_button);
    }
 
+   private boolean checkLocationPermission()
+   {
+      if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED)
+      {
+         // Should we show an explanation?
+         if (ActivityCompat.shouldShowRequestPermissionRationale(
+               this, Manifest.permission.ACCESS_FINE_LOCATION))
+         {
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+            new AlertDialog.Builder(this)
+                    .setTitle("Location Permission")
+                    .setMessage("Grant location permissions?")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener()
+                    {
+                       @Override
+                       public void onClick(DialogInterface dialogInterface, int i)
+                       {
+                          //Prompt the user once explanation has been shown
+                          ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    MY_PERMISSIONS_REQUEST_LOCATION);
+                       }
+                    })
+                    .create()
+                    .show();
+         }
+         else
+         {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(
+             this,
+             new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+             MY_PERMISSIONS_REQUEST_LOCATION);
+         }
+         return false;
+       }
+      else
+      {
+        return true;
+      }
+   }
 
+   @Override
+   public void onRequestPermissionsResult(
+      int requestCode, String permissions[], int[] grantResults)
+   {
+      switch(requestCode)
+      {
+         case MY_PERMISSIONS_REQUEST_LOCATION:
+         {
+            // If request is cancelled, the result arrays are empty.
+            if((grantResults.length > 0)
+                    && (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+            {
+               // permission was granted, yay! Do the
+               // location-related task you need to do.
+               if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED)
+               {
+//                    //Request location updates:
+//                    locationManager.requestLocationUpdates(provider, 400, 1, this);
+               }
+            }
+            else
+            {
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+            }
+            return;
+         }
+      }
+   }
 }
